3324a5dcab047f41171422f80ac9ef28c5ae66fd
2LargeFeet <michael.gombos@gmail.com>
2022-12-27 15:59:13 -0500

    dynamically get IP address and update ami

--- instance.tf
+++ instance.tf
@@ -1,139 +1,153 @@
-variable "local_ip" {}
-variable "cidr_block" {}
-variable "cidr_subnet" {}
-variable "private_key" {}
-variable "private_key_file" {}
+variable "local_ip" {
+}
+
+variable "cidr_block" {
+}
+
+variable "cidr_subnet" {
+}
+
+variable "private_key" {
+}
+
+variable "private_key_file" {
+}
 
 data "aws_ami" "ubuntu" {
-  most_recent           = true
-  owners                = ["099720109477"] # Canonical
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
   filter {
-    name              = "name"
-    values            = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
-    name              = "virtualization-type"
-    values            = ["hvm"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
 
 resource "aws_vpc" "main" {
-  cidr_block              = "${var.cidr_block}"
-  enable_dns_hostnames    = true
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "external" {
-  vpc_id                  = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name                  = "external"
+    Name = "external"
   }
 }
 
 resource "aws_subnet" "external" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "${var.cidr_subnet}"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_subnet
 
   tags = {
-    Name                  = "external"
+    Name = "external"
   }
 }
 
 resource "aws_route_table" "external" {
-  vpc_id                    = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block              = "0.0.0.0/0"
-    gateway_id              = "${aws_internet_gateway.external.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.external.id
   }
 
   tags = {
-    Name                    = "external"
+    Name = "external"
   }
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id                 = "${aws_subnet.external.id}"
-  route_table_id            = "${aws_route_table.external.id}"
+  subnet_id      = aws_subnet.external.id
+  route_table_id = aws_route_table.external.id
 }
 
-
 resource "aws_security_group" "restrict" {
-  name                    = "restrict"
-  description             = "restrict access to server"
-  vpc_id                  = "${aws_vpc.main.id}"
+  name        = "restrict"
+  description = "restrict access to server"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port             = 22
-    to_port               = 22
-    protocol              = "tcp"
-    cidr_blocks           = ["${var.local_ip}"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
 
   ingress {
-    from_port             = 1194
-    to_port               = 1194
-    protocol              = "udp"
-    cidr_blocks           = ["${var.local_ip}"]
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
 
   ingress {
-    from_port             = 80
-    to_port               = 80
-    protocol              = "tcp"
-    cidr_blocks           = ["${var.local_ip}"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
 
   ingress {
-    from_port             = 443
-    to_port               = 443
-    protocol              = "tcp"
-    cidr_blocks           = ["${var.local_ip}"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
 
   egress {
-    from_port             = 0
-    to_port               = 0
-    protocol              = "-1"
-    cidr_blocks           = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "vpn" {
-  ami                     = "${data.aws_ami.ubuntu.id}"
-  instance_type           = "t2.micro"
-  key_name                = "${var.private_key}"
-  vpc_security_group_ids  = ["${aws_security_group.restrict.id}"]
-  subnet_id               = "${aws_subnet.external.id}"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = var.private_key
+  vpc_security_group_ids      = [aws_security_group.restrict.id]
+  subnet_id                   = aws_subnet.external.id
   associate_public_ip_address = true
 
   provisioner "remote-exec" {
     inline = [
       "sleep 30",
+      "sudo add-apt-repository ppa:deadsnakes/ppa -y",
       "sudo apt update",
       "sudo apt upgrade -y",
       "sudo apt install aptitude -y",
-      "sudo apt install python-pip -y",
+      "sudo apt install python3-pip -y",
       "sudo pip install ansible",
       "git clone https://github.com/2LargeFeet/newipeveryday.git",
-      "sudo ansible-playbook newipeveryday/ipeveryday.yml --extra-vars='{\"server_ip\": ${aws_instance.vpn.public_ip}}'"
+      "sudo ansible-playbook newipeveryday/ipeveryday.yml --extra-vars='{\"server_ip\": ${aws_instance.vpn.public_ip}}'",
     ]
 
     connection {
-      type                = "ssh"
-      user                = "ubuntu"
-      private_key         = "${file(var.private_key_file)}"
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_file)
     }
   }
 
   provisioner "local-exec" {
-    command = "scp -i vpn.pem -o 'StrictHostKeyChecking no' ubuntu@${aws_instance.vpn.public_ip}:newipeveryday/client-config/client.ovpn client.ovpn"
+    command     = "scp -i vpn.pem -o 'StrictHostKeyChecking no' ubuntu@${aws_instance.vpn.public_ip}:newipeveryday/client-config/client.ovpn client.ovpn"
     interpreter = ["PowerShell", "-Command"]
   }
 }
 
 output "ip" {
-  value = "${aws_instance.vpn.public_ip}"
+  value = aws_instance.vpn.public_ip
 }
+
