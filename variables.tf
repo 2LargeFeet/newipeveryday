f2474d687014f56c7fe1f6ca0f46a984a52b807d
2LargeFeet <michael.gombos@gmail.com>
2022-12-29 10:32:25 -0500

    adding root module files

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
+
+variable "private_key_file" {
+}
+
+variable "private_key" {
+}
+
+variable profile_name {
+    default = "default"
+}
+
+variable cidr_block {
+    default = "10.23.0.0/16"
+}
+
+variable cidr_subnet {
+    default = "10.23.0.0/24"
+}
